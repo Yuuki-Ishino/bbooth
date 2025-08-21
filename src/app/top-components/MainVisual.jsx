f76e14292e8de73d0326426ef6b6e7907f9880b3
Yuuki-Ishino <yyuuuukkii0914@gmail.com>
2025-08-21 23:49:11 +0900

    fix mobile bg-size

--- src/app/top-components/MainVisual.jsx
+++ src/app/top-components/MainVisual.jsx
@@ -1,9 +1,9 @@
 function MainVisual() {
 	return (
 		<section
-			className="bg-[url('/images/top-image.jpg')] w-full bg-fixed bg-cover bg-center min-h-screen"
+			className="bg-[url('/images/top-image.jpg')] h-[600px] w-full bg-fixed bg-cover bg-center"
 		>
-			<div className="w-[90%] max-w-[1280px] mx-auto h-[600px] relative">
+			<div className="w-[90%] max-w-[1280px] mx-auto h-full relative">
 				<div className="absolute bottom-[100px] left-0">
 					<p className="text-[70px] font-bold text-white mb-6">Bbooth</p>
 					<p className="text-[24px] font-black text-white">
