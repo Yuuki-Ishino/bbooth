6cb8cbe85da829bf1a874cc36ed26730dcfabbca
Yuuki-Ishino <yyuuuukkii0914@gmail.com>
2025-08-23 17:15:43 +0900

    add active to button

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -6,7 +6,7 @@ export default function Button({ href, children }) {
         inline-block w-[270px] h-[60px] text-[20px] font-bold 
         text-white border border-white rounded-full 
         text-center leading-[60px] transition-colors duration-200
-        hover:bg-white hover:text-black
+        hover:bg-white hover:text-black active:bg-white active:text-black
       "
     >
       {children}
--- src/app/top-components/ActivitySection.jsx
+++ src/app/top-components/ActivitySection.jsx
@@ -17,7 +17,7 @@ function ActivitySection({ subtitle, title, items }) {
               <img
                 src={item.image}
                 alt={item.alt}
-                className="w-full rounded-[20px] hover:opacity-70 "
+                className="w-full rounded-[20px] hover:opacity-70 active:opaicty-70"
               />
               <p className="bg-white/80 text-black font-bold inline-block absolute bottom-[6px] left-1/2 -translate-x-1/2 px-2.5 py-1 rounded-[10px]">
                 {item.title}
