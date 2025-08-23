e5cef2fc12bea4ddac4969fc75afe6449fe4e9c7
Yuuki-Ishino <yyuuuukkii0914@gmail.com>
2025-08-24 05:23:29 +0900

    fix: [id]/page.jsx fetch url

--- src/app/activities/[id]/page.jsx
+++ src/app/activities/[id]/page.jsx
@@ -1,27 +1,30 @@
 export default async function ActivityDetail({ params }) {
   try {
-	const res = await fetch(`http://localhost:3000/data/activities.json`);
-	if (!res.ok) throw new Error("データの取得に失敗");
+    const baseUrl =
+      process.env.NODE_ENV === "development" 
+        ? "http://localhost:3000" : "https://bbooth.vercel.app";
+    const res = await fetch(`${baseUrl}/data/activities.json`);
+    if (!res.ok) throw new Error("データの取得に失敗");
 
-	const data = await res.json();
-	const activity = data.items.find(item => item.id.toString() === params.id);
+    const data = await res.json();
+    const activity = data.items.find(item => item.id.toString() === params.id);
 
-	if (!activity) {
-		return <p className="p-16 text-white">投稿が見つかりませんでした。</p>;
-  }
+    if (!activity) {
+      return <p className="p-16 text-white">投稿が見つかりませんでした。</p>;
+    }
 
-  return (
-    <section className="pt-16">
-      <img
-        src={activity.image}
-        alt={activity.alt}
-        className="w-full h-64 object-cover rounded"
-      />
-      <h1 className="text-white text-3xl font-bold mt-4">{activity.title}</h1>
-      <p className="text-white">{activity.date}</p>
-      <p className="text-white mt-4">{activity.description}</p>
-    </section>
-  );
+    return (
+      <section className="pt-[70px]">
+        <img
+          src={activity.image}
+          alt={activity.alt}
+          className="w-full h-64 object-cover rounded"
+        />
+        <h1 className="text-white text-3xl font-bold mt-4">{activity.title}</h1>
+        <p className="text-white ">{activity.date}</p>
+        <p className="text-white  mt-4">{activity.description}</p>
+      </section>
+    );
   } catch (error) {
     return <p className="p-16 text-white">データの読み込み中にエラーが発生しました。</p>;
   }
