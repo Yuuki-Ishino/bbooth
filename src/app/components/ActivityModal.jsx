9368aff90a6d1bd7e562b966305256a100a432f6
Yuuki-Ishino <yyuuuukkii0914@gmail.com>
2025-09-02 23:09:31 +0900

    add: RemoveScroll lib for safari chrome

--- src/app/components/ActivityModal.jsx
+++ src/app/components/ActivityModal.jsx
@@ -1,61 +1,56 @@
 "use client";
 
 import React, {useEffect} from "react";
+import { RemoveScroll } from "react-remove-scroll";
 
 export default function ActivityModal({ activity, onClose }) {
-	useEffect(() => {
-		document.body.style.overflow = "hidden";
-
-		return () => {
-			document.body.style.overflow = "auto";
-		};
-	}, []);
-
-  if (!activity) return null;
+	if (!activity) return null;
 
   return (
-    <div
-      className="fixed inset-0 z-50 flex items-center justify-center bg-black/80"
-      onClick={onClose} // 背景クリックで閉じる
-    >
-      <div
-        className="bg-[#181619] text-white w-[95%] max-w-[800px] max-h-[80vh] overflow-auto rounded-lg p-4 relative"
-        onClick={(e) => e.stopPropagation()} // 内側クリックは閉じない
+    <RemoveScroll>
+            <div
+        className="fixed inset-0 z-50 flex items-center justify-center bg-black/80"
+        onClick={onClose} // 背景クリックで閉じる
       >
-        {/* 閉じるボタン */}
-        <button
-          className="absolute top-2 right-2 text-white text-3xl font-bold"
-          onClick={onClose}
+        <div
+          className="bg-[#181619] text-white w-[95%] max-w-[800px] max-h-[80vh] overflow-auto rounded-lg p-4 relative"
+          onClick={(e) => e.stopPropagation()} // 内側クリックは閉じない
         >
-					X
-        </button>
+          {/* 閉じるボタン */}
+          <button
+            className="absolute top-2 right-2 text-white text-3xl font-bold"
+            onClick={onClose}
+          >
+            X
+          </button>
 
-        {/* ヘッダー画像 */}
-        <img
-          src={activity.image}
-          alt={activity.alt}
-          className="w-full h-64 object-cover rounded-lg"
-        />
+          {/* ヘッダー画像 */}
+          <img
+            src={activity.image}
+            alt={activity.alt}
+            className="w-full h-64 object-cover rounded-lg"
+          />
 
-        {/* タイトル */}
-        <h1 className="text-2xl font-bold py-4 border-b border-white">{activity.title}</h1>
+          {/* タイトル */}
+          <h1 className="text-2xl font-bold py-4 border-b border-white">{activity.title}</h1>
 
-        {/* 投稿情報 */}
-        <div className="py-4 space-y-3 border-b border-white">
-          <div className="flex items-center gap-2">
-            <p>活動日: {activity.date}</p>
+          {/* 投稿情報 */}
+          <div className="py-4 space-y-3 border-b border-white">
+            <div className="flex items-center gap-2">
+              <p>活動日: {activity.date}</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <p>活動場所: {activity.location || "宇宙"}</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <p>参加人数: {activity.num_people || "みんな"}</p>
+            </div>
           </div>
-          <div className="flex items-center gap-2">
-            <p>活動場所: {activity.location || "宇宙"}</p>
-          </div>
-          <div className="flex items-center gap-2">
-            <p>参加人数: {activity.num_people || "みんな"}</p>
-          </div>
+
+          {/* 詳細説明 */}
+          <p className="mt-6 leading-relaxed">{activity.description}</p>
         </div>
-
-        {/* 詳細説明 */}
-        <p className="mt-6 leading-relaxed">{activity.description}</p>
       </div>
-    </div>
+    </RemoveScroll>
   );
 }
